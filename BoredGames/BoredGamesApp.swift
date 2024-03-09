d441e2cfa3e66845eecaed276324cf94457a2ae0
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2024-03-08 18:03:57 -0800

    Initial Commit

---
+++ BoredGames/BoredGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredGamesApp.swift
+//  BoredGames
+//
+//  Created by Ayush Raman on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BoredGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoredGamesUITests/BoredGamesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BoredGamesUITestsLaunchTests.swift
+//  BoredGamesUITests
+//
+//  Created by Ayush Raman on 3/8/24.
+//
+
+import XCTest
+
+final class BoredGamesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
